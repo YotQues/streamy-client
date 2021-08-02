78115f5656340a168f415b5437547f0e05a2ceb8
Yotam Aran <yotam.sound@gmail.com>
2021-08-02 14:34:20 +0300

    delete stream works

--- src/Components/streams/StreamDelete.js
+++ src/Components/streams/StreamDelete.js
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { fetchStream, deleteStream } from '../../actions';
+import history from '../../history';
+
 import Modal from '../Modal';
 
-const StreamDelete = (props) => {
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  )
-  return (
-    <div>
+class StreamDelete extends Component {
+
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  doDelete = async (id) => {
+    await this.props.deleteStream(id);
+    history.push("/");
+  }
+
+  renderActions = () => {
+    const { id } = this.props.match.params;
+    return (
+      <>
+        <button onClick={() => this.doDelete(id)} className="ui button negative">Delete</button>
+        <Link to="/" className="ui button">Cancel</Link>
+      </>
+    );
+  }
+
+  renderContent = () => {
+    if (!this.props.stream)
+      return 'Are you sure you want to delete this stream?';
+
+    return `Are you sure you want to delete this stream with title "${this.props.stream.title}"?`;
+  }
+
+  render() {
+    return (
       <Modal
         title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={() => history.push('/')}
       />
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchStream, deleteStream }
+)(StreamDelete);
--- src/actions/index.js
+++ src/actions/index.js
@@ -48,8 +48,8 @@ export const editStream = (id, formValues) =>
     dispatch({ type: EDIT_STREAM, payload: response.data });
   };
 
-export const deleteStream = (id) =>
+export const deleteStream = id =>
   async dispatch => {
     await streams.delete(`streams/${id}`);
     dispatch({ type: DELETE_STREAM, payload: id });
-  }
+  };
